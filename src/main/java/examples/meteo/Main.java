3d32e87ef1e90f0f714cba37f285dc82901ca9f7
KiTSune MiDSun <118630098+Ask1509@users.noreply.github.com>
2023-05-24 21:41:48 +0300

    Add files via upload

---
+++ src/main/java/examples/meteo/Main.java
@@ -0,0 +1,20 @@
+package examples.meteo;
+
+public class Main {
+
+    // Есть библиотека сбора данных от датчиков. Все они были куплены в комплекте с этой библиотекой.
+    // Поступил новый датчик от другой компании, интерфейс которого отличается от предыдущих.
+    // Надо включить этот датчик в систему.
+    public static void main(String[] args) {
+        MeteoStore meteoDb = new MeteoStore();
+
+        MeteoSensor ms200_1 = new MS200(1);
+        meteoDb.save(ms200_1);
+
+        // Здесь надо вызвать метод getData у класса ST500Info. Полученные данные отправить в метод save объекта meteoDb
+        ST500Info st500 = new ST500Info();
+        ST500Adapter adapter = new ST500Adapter(st500.getData());
+        meteoDb.save(adapter);
+
+    }
+}
